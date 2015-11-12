a0277481ab5f7b897f90d4d5c0377cfc1acea681
DePascual <carolinadepascualjimenez@gmail.com>
2015-11-12 18:47:25 +0100

    Empezando con finalizar pedido. HILOS

---
+++ Agapea2/Agapea2/App_Code/controlador/controlador_Email.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Agapea2.App_Code.controlador;
+using Agapea2.App_Code.modelo;
+using System.Net.Mail;
+
+
+namespace Agapea2.App_Code.controlador
+{
+    public class controlador_Email
+    {
+        public bool MandarEmail(Usuario user) //Donde estan los datos de este usuario
+        {
+            MailMessage mensajeAMandar = this.CrearEmail(user);
+            SmtpClient server = new SmtpClient("smtp.gmail.com", 587);
+            try
+            {
+                server.Send(mensajeAMandar);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private MailMessage CrearEmail(Usuario user)
+        {
+            //dentro de user habra un carrito con una coleccion de libros
+            Dictionary<string, Libro> librosCarrito = user.obtenerUltimoCarrito();
+            controlador_generar_PDF generadorFactura = new controlador_generar_PDF();
+
+            MailMessage mail = new MailMessage();
+            mail.To.Add(new MailAddress(user.emailUsuario));
+            mail.From = "agapea@gmail.com";
+            mail.Subject = "Factura con fecha...";
+            mail.Attachments.Add(new Attachment(generadorFactura.CrearDocPDF(librosCarrito))); //la ruta del pdf, hay que guardarlo
+
+            return mail;
+        }
+    }
+}
---
+++ Agapea2/Agapea2/App_Code/controlador/controlador_generar_PDF.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Spire.Pdf;
+using Agapea2.App_Code.modelo;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agapea2.App_Code.controlador
+{
+    public class controlador_generar_PDF
+    {
+
+        public PdfDocument CrearDocPDF(Dictionary<string, Libro> coleccionLibrosCarrito)
+        {
+            String facturaHTML = GenerarFacturaEnHTML(coleccionLibrosCarrito);
+
+            PdfDocument miFactura = new PdfDocument();
+            Task generadorPDF = new Task(() => miFactura.LoadFromHTML(facturaHTML, false, true, true));
+            generadorPDF.Start();
+            //si en el pdf lo quiero grabar en un fichero en el servidor llamaria a miFactura.SaveToFile("nombre_fichero.pdf")
+            generadorPDF.Wait(); //...nos aseguramos que el hilo acabe         
+            return miFactura;
+
+        }
+
+        private String GenerarFacturaEnHTML(Dictionary<string,Libro> coleccionLibrosCarrito)
+        {
+            StringBuilder midocHTML = new StringBuilder();
+            midocHTML.Append("<html><head><title>FACTURA DE  CLIENTE</title></head>");
+            midocHTML.Append("<body><img src='" + HttpContext.Current.Request.MapPath("/imagenes/encabezado_inicio.png"));
+            midocHTML.Append("<table>");
+            midocHTML.Append( (from unLibro in coleccionLibrosCarrito select
+                                       "<tr>" +
+                                       "<td>" + unLibro.Titulo + "</td>" +
+                                       "<td>" + unLibro.Autor + "</td></td>").Single());
+            midocHTML.Append("</table></body></html>");
+            return midocHTML.ToString();
+        }
+    }
+}
