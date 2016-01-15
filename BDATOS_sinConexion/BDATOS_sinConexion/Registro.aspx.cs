eb92d82356321d9881dfc732a8c87fc0fe720edb
DePascual <carolinadepascualjimenez@gmail.com>
2016-01-15 19:29:39 +0100

    Hay un error

---
+++ BDATOS_sinConexion/BDATOS_sinConexion/Registro.aspx.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace BDATOS_sinConexion
+{
+    public partial class Registro : System.Web.UI.Page
+    {
+        private DataSet miDataSet = new DataSet();
+        private SqlConnection conexionBD = new SqlConnection(ConfigurationManager.ConnectionStrings["miConexionSQLSERVER"].ConnectionString);
+        private SqlDataAdapter adaptadorUsuarios;
+        private SqlCommandBuilder builderUsuarios;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!this.IsPostBack)
+            {
+                using (this.conexionBD = new SqlConnection(ConfigurationManager.ConnectionStrings["miConexionSQLSERVER"].ConnectionString))
+                {
+                    try
+                    {
+                        this.conexionBD.Open();
+                        adaptadorUsuarios = new SqlDataAdapter("SELECT * FROM Clientes", this.conexionBD);
+                        builderUsuarios = new SqlCommandBuilder(adaptadorUsuarios);
+                        adaptadorUsuarios.Fill(miDataSet, "Clientes");
+                    }
+                    catch (SqlException ex)
+                    {
+                    }
+                    finally { this.conexionBD.Close();
+                        Session["miDataSet"] = this.miDataSet;
+                    }
+                }
+            }
+            else
+            {
+                this.miDataSet = (DataSet)Session["miDataSet"];
+            }       
+        }
+
+        protected void buttonRegistro_Click(object sender, EventArgs e)
+        {
+            DataTable miTabla = miDataSet.Tables["Clientes"];
+            miTabla.Rows.Add(new object[] { this.textBoxNIF.Text, this.textBoxNombre.Text, this.textBoxApellidos.Text, this.textBoxDireccion.Text, this.textBoxLocalidad.Text, this.textBoxProvincia.Text });
+        }
+
+        protected void buttonModificar_Click(object sender, EventArgs e)
+        {
+            DataTable miTabla = miDataSet.Tables["Clientes"];
+            foreach (DataRow fila in miTabla.Rows)
+            {
+                if ((string)fila["NIF"] == this.textBoxNIF.Text)
+                {
+                    fila["Nombre"] = this.textBoxNombre.Text;
+                    fila["Apellidos"] = this.textBoxApellidos.Text;
+                    fila["Direccion"] = this.textBoxDireccion.Text;
+                    fila["Localidad"] = this.textBoxLocalidad.Text;
+                    fila["Provincia"] = this.textBoxProvincia.Text;
+                }
+            }
+        }
+
+        protected void buttonBorrar_Click(object sender, EventArgs e)
+        {
+            DataTable miTabla = miDataSet.Tables["Clientes"];
+            foreach (DataRow fila in miTabla.Rows)
+            {
+                if ((string)fila["NIF"] == this.textBoxNIF.Text)
+                {
+                    miTabla.Rows.Remove(fila);
+                }
+            }
+
+            //miTabla.Rows.Remove(miTabla.Rows.Cast<DataRow>().Where(fila => fila["NIF"] == this.textBoxNIF.Text));
+
+        }
+
+        protected void buttonFinalizar_Click(object sender, EventArgs e)
+        {
+            using (this.conexionBD = new SqlConnection(ConfigurationManager.ConnectionStrings["miConexionSQLSERVER"].ConnectionString))
+            {
+                try
+                {
+                    this.conexionBD.Open();
+                    this.adaptadorUsuarios.Update(this.miDataSet.Tables["Clientes"]);
+                    this.miDataSet.AcceptChanges();
+
+                }
+                catch (SqlException ex)
+                {
+
+                }
+                finally
+                {
+                    this.conexionBD.Close();
+                }
+            }
+        }
+    }
+}
---
+++ BDATOS_sinConexion/BDATOS_sinConexion/Registro.aspx.designer.cs
@@ -0,0 +1,177 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace BDATOS_sinConexion {
+    
+    
+    public partial class Registro {
+        
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// Control Label1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+        
+        /// <summary>
+        /// Control Label2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label2;
+        
+        /// <summary>
+        /// Control textBoxNombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox textBoxNombre;
+        
+        /// <summary>
+        /// Control Label3.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label3;
+        
+        /// <summary>
+        /// Control textBoxApellidos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox textBoxApellidos;
+        
+        /// <summary>
+        /// Control Label4.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label4;
+        
+        /// <summary>
+        /// Control textBoxNIF.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox textBoxNIF;
+        
+        /// <summary>
+        /// Control Label5.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label5;
+        
+        /// <summary>
+        /// Control textBoxDireccion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox textBoxDireccion;
+        
+        /// <summary>
+        /// Control Label6.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label6;
+        
+        /// <summary>
+        /// Control textBoxLocalidad.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox textBoxLocalidad;
+        
+        /// <summary>
+        /// Control Label7.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label7;
+        
+        /// <summary>
+        /// Control textBoxProvincia.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox textBoxProvincia;
+        
+        /// <summary>
+        /// Control buttonRegistro.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button buttonRegistro;
+        
+        /// <summary>
+        /// Control buttonModificar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button buttonModificar;
+        
+        /// <summary>
+        /// Control buttonBorrar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button buttonBorrar;
+        
+        /// <summary>
+        /// Control buttonFinalizar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button buttonFinalizar;
+    }
+}
